8757e88b5f2b66062623e814ac01aefca4396d4b
martlandh <HarryMartland@Rentalcars.com>
2019-04-08 20:45:07 +0100

    removed hashtag from messages

--- messages.js
+++ messages.js
@@ -1,12 +1,12 @@
 exports.chain = [
-    (username, days) => `Wow @${username} you have #LearnToCode ${days} days in a row! Keep it up!`,
-    (username, days) => `${days} days in a row! Are you some kind of #LearnToCode machine @${username}?`
+    (username, days) => `Wow @${username} you have been learning to code ${days} days in a row! Keep it up!`,
+    (username, days) => `${days} days in a row! Are you some kind of learning machine @${username}?`
 ];
 
 exports.total = [
-    (username, days) => `Wow you have #LearnToCode a total of ${days} days @${username}`,
-    (username, days) => `Hey #LearnToCode @${username} has been learning for ${days} days`,
-    (username, days) => `Guess who has been learning for ${days} days?... its @${username} #LearnToCode`,
-    (username, days) => `Want to #LearnToCode? Follow @${username} as they have been doing it for ${days} days`,
-    (username, days) => `Keep up the good work @${username}! That's ${days} days trying to #LearnToCode`
+    (username, days) => `Wow you have been learning a total of ${days} days @${username}`,
+    (username, days) => `Hey all, @${username} has been learning for ${days} days`,
+    (username, days) => `Guess who has been learning for ${days} days?... its @${username}`,
+    (username, days) => `Want to learn to code? Follow @${username} as they have been doing it for ${days} days`,
+    (username, days) => `Keep up the good work @${username}! That's ${days} days learning to code`
 ];
